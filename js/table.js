e3f5ae55c24b481df8e2fddd2fcb018e02582d24
Nigel Gilbert <n.gilbert@surrey.ac.uk>
2021-05-29 12:09:26 +0100

    added hidden column; better headers for tickCross

--- js/table.js
+++ js/table.js
@@ -272,19 +272,13 @@ function initialiseFactorTable() {
 		columnHeaderVertAlign: 'bottom',
 		columns: [
 			{
-				title: '<input id="select-all" type="checkbox"/>&nbsp;Select',
+				title: `Select&nbsp;
+					<span  id="select-all"><span class="checkbox-box-off">${svg('cross')}</span>
+			  		<span class="checkbox-box-on">${svg('tick')}</span></span>`,
 				field: 'selection',
 				hozAlign: 'center',
 				formatter: 'tickCross',
-				formatterParams: {
-					allowTruthy: true,
-					tickElement: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
-					<path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
-				  </svg>`,
-					crossElement: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-app" viewBox="0 0 16 16">
-					<path d="M11 2a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V5a3 3 0 0 1 3-3h6zM5 1a4 4 0 0 0-4 4v6a4 4 0 0 0 4 4h6a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4H5z"/>
-				  </svg>`,
-				},
+				formatterParams: tickCrossFormatter(),
 				cellClick: tickToggle,
 				headerVertical: true,
 			},
@@ -303,6 +297,17 @@ function initialiseFactorTable() {
 						field: 'shape',
 						editor: 'select',
 						editorParams: {values: {box: 'box', ellipse: 'ellipse', circle: 'disk', text: 'none'}},
+					},
+					{
+						title: `Hidden&nbsp;
+						<span  id="hide-all"><span class="checkbox-box-off">${svg('cross')}</span>
+						  <span class="checkbox-box-on">${svg('tick')}</span></span>`,
+						field: 'hidden',
+						hozAlign: 'center',
+						formatter: 'tickCross',
+						formatterParams: tickCrossFormatter(),
+						cellClick: tickToggle,
+						headerVertical: true,
 					},
 					{
 						title: 'Relative Size',
@@ -393,9 +398,16 @@ function initialiseFactorTable() {
 		}
 	}
 	window.factorsTable = factorsTable;
-	listen('select-all', 'change', (e) => {
+	listen('select-all', 'click', (e) => {
+		let ticked = headerTickToggle(e, '#select-all');
 		factorsTable.getRows('active').forEach((row) => {
-			row.update({selection: e.target.checked});
+			row.update({selection: !ticked});
+		});
+	});
+	listen('hide-all', 'click', (e) => {
+		let ticked = headerTickToggle(e, '#hide-all');
+		factorsTable.getRows('active').forEach((row) => {
+			row.update({hidden: !ticked});
 		});
 	});
 	return factorsTable;
@@ -404,7 +416,47 @@ function initialiseFactorTable() {
 function tickToggle(e, cell) {
 	cell.setValue(!cell.getValue());
 }
+function headerTickToggle(e, selector) {
+	e.stopPropagation();
+	let off = document.querySelector(selector + ' .checkbox-box-off');
+	let on = document.querySelector(selector + ' .checkbox-box-on');
+	let ticked = off.style.display == 'none';
+	if (ticked) {
+		off.style.display = 'inline';
+		on.style.display = 'none';
+	} else {
+		on.style.display = 'inline';
+		off.style.display = 'none';
+	}
+	return ticked;
+}
 
+function tickCrossFormatter() {
+	return {
+		allowTruthy: true,
+		tickElement: svg('tick'),
+		crossElement: svg('cross'),
+	};
+}
+
+function svg(icon) {
+	switch (icon) {
+		case 'tick':
+			return `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-square" viewBox="0 0 16 16">
+		<path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+		<path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z"/>
+	  </svg>`;
+		case 'cross':
+			return `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-square" viewBox="0 0 16 16">
+		<path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+	  </svg>`;
+		case 'close':
+			return `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-square" viewBox="0 0 16 16">
+		<path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+		<path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+	  </svg>`;
+	}
+}
 /**
  * spread some deep values to the top level to suit the requirements of the Tabulator package better
  * NB: any such converted values cannot then be edited without special attention (in updateEdgeCellData)
@@ -514,8 +566,7 @@ function convertNodeBack(node, field, value) {
 			node.font.size = parseFloat(value);
 			break;
 		case 'size':
-			if (value == '--')
-				node.scaling.label.enabled = false;
+			if (value == '--') node.scaling.label.enabled = false;
 			else {
 				node.scaling.label.enabled = true;
 				node.value = parseFloat(value);
@@ -854,7 +905,7 @@ function setUpFilter() {
 	let i = 0;
 	openTable.getColumns().forEach((colComp) => {
 		let def = colComp.getDefinition();
-		if (def.formatter != 'color'  && def.field != 'selection')
+		if (def.formatter != 'color' && def.field != 'selection')
 			// cannot sort by color
 			select[i++] = new Option(def.title, def.field);
 	});
@@ -876,10 +927,7 @@ function setUpFilter() {
 		<option value="!=">!=</option>
   	</select>
 	  <input id="filter-value" type="text">
-	  <button id= "filter-close"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-square" viewBox="0 0 16 16">
-	  <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-	  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-	</svg></button>`
+	  <button id= "filter-close">${svg('close')}</button>`
 	);
 	listen('filter-field', 'change', updateFilter);
 	listen('filter-type', 'change', updateFilter);
