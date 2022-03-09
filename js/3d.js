de8ab9db94bbbb18fa68d87499dd65f3a85a5bb9
Nigel Gilbert <n.gilbert@surrey.ac.uk>
2022-03-09 22:27:45 +0000

    Add light and dark modes to 3D

--- js/3d.js
+++ js/3d.js
@@ -1,3 +1,11 @@
+/**
+ *
+ * PRSM 3D map view
+ * generates a 'three dimensional'view of a PRSM map, build on threejs
+ *
+ *
+ * */
+
 import * as Y from 'yjs'
 import {WebsocketProvider} from 'y-websocket'
 import {Network} from 'vis-network/peer/'
@@ -23,8 +31,6 @@ var ySamplesMap // shared map of styles
 var myNameRec // my name etc.
 var loadingDelayTimer // timer to delay the start of the loading animation for few moments
 var graph // the 3D map
-var graphNodes
-var graphEdges
 
 window.addEventListener('load', () => {
 	loadingDelayTimer = setTimeout(() => {
@@ -46,10 +52,11 @@ function startY() {
 	else room = room.toUpperCase()
 	debug = [url.searchParams.get('debug')]
 	document.title = document.title + ' ' + room
+	// don't bother with a local storage provider
 	const wsProvider = new WebsocketProvider(websocket, 'prsm' + room, doc)
 	wsProvider.on('sync', () => {
 		console.log(exactTime() + ' remote content loaded')
-		display()
+		display('white')
 	})
 	wsProvider.on('status', (event) => {
 		console.log(exactTime() + event.status + (event.status == 'connected' ? ' to' : ' from') + ' room ' + room) // logs when websocket is "connected" or "disconnected"
@@ -73,16 +80,15 @@ function startY() {
 	window.yNetMap = yNetMap
 	window.ySamplesMap = ySamplesMap
 
+	// we only read changes made by other clients; this client never writes anything to the shared document
 	yNodesMap.observe(() => {
 		if (graph) {
-			convertData()
-			graph.graphData({nodes: graphNodes, links: graphEdges})
+			graph.graphData(convertData())
 		}
 	})
 	yEdgesMap.observe(() => {
 		if (graph) {
-			convertData()
-			graph.graphData({nodes: graphNodes, links: graphEdges})
+			graph.graphData(convertData())
 		}
 	})
 	ySamplesMap.observe((event) => {
@@ -137,9 +143,8 @@ function cancelLoading() {
  * @returns Object
  */
 function convertNode(node) {
-	return {id: node.id, label: node.label, color: node.color.background, fontColor: node.font.color, val: 5} //  * Math.log(2 + node.bc)}
+	return {id: node.id, label: node.label, color: node.color.background, fontColor: node.font.color, val: 5}
 }
-
 /**
  * Convert an edge from the normal PRSM format to the one required by the 3d display
  * @param {object} edge
@@ -149,22 +154,187 @@ function convertEdge(edge) {
 	return {
 		source: edge.from,
 		target: edge.to,
-		color: standardize_color(edge.color.color) === '#000000' ? 'white' : edge.color.color,
+		// black links are invisible on a black background, so change them to grey,
+		// so that they are visible on either white or black backgrounds
+		color: standardize_color(edge.color.color) === '#000000' ? 'lightgrey' : edge.color.color,
 	}
 }
 /**
  * Convert the map data from PRSM format to 3d display format
  * Avoid cluster nodes
+ * Add lists of links and neighbours to each node
+ * @return {object} {nodes; links}
  */
 function convertData() {
-	graphNodes = Array.from(yNodesMap.values())
+	let graphNodes = Array.from(yNodesMap.values())
 		.filter((n) => !n.isCluster)
 		.map((n) => {
 			return convertNode(n)
 		})
-	graphEdges = Array.from(yEdgesMap.values()).map((e) => {
+	let graphEdges = Array.from(yEdgesMap.values()).map((e) => {
 		return convertEdge(e)
 	})
+	// note neighbouring nodes and links to those for each node
+	// (used for highlighting links when hovering over nodes)
+	graphEdges.forEach((link) => {
+		const a = graphNodes.find((n) => n.id === link.source)
+		const b = graphNodes.find((n) => n.id === link.target)
+		!a.neighbors && (a.neighbors = [])
+		!b.neighbors && (b.neighbors = [])
+		a.neighbors.push(b)
+		b.neighbors.push(a)
+		!a.links && (a.links = [])
+		!b.links && (b.links = [])
+		a.links.push(link)
+		b.links.push(link)
+	})
+	return {nodes: graphNodes, links: graphEdges}
+}
+
+/**
+ * Display the map as a 3D network
+ * @param {string} backColor - background color: white or black
+ */
+function display(backColor) {
+	cancelLoading()
+	let threeDGraphDiv = elem('3dgraph')
+	let width = threeDGraphDiv.clientWidth
+	let height = threeDGraphDiv.clientHeight
+	const highlightNodes = new Set()
+	const highlightLinks = new Set()
+	let hoverNode = null
+
+	graph = ForceGraph3D()(threeDGraphDiv)
+		.width(width)
+		.height(height)
+		.graphData(convertData())
+		.showNavInfo(false)
+		.linkWidth((link) => (highlightLinks.has(link) ? 1 : 0))
+		.linkDirectionalArrowLength(2)
+		.linkDirectionalArrowRelPos(1)
+		.backgroundColor(backColor)
+		.nodeOpacity(1.0)
+		.linkOpacity(0.7)
+		.linkDirectionalParticles(5)
+		.nodeThreeObjectExtend(false)
+		.nodeThreeObject((node) => {
+			// extend node with text sprite
+			const sprite = new SpriteText(`${node.label}`)
+			sprite.color = node.fontColor
+			sprite.textHeight = 3
+			sprite.padding = 1
+			sprite.backgroundColor = node.color
+			sprite.borderWidth = 1
+			sprite.borderRadius = 4
+			return sprite
+		})
+		.onNodeDragEnd((node) => {
+			if (node.fx) {
+				// unfix fixed node
+				node.fx = null
+				node.fy = null
+				node.fz = null
+			} else {
+				// fix nodes if they have been dragged
+				node.fx = node.x
+				node.fy = node.y
+				node.fz = node.z
+			}
+		})
+		.onNodeHover(doHover)
+		.onNodeClick((node) => {
+			// Aim at node from outside it
+			const distance = 80
+			const distRatio = 1 + distance / Math.hypot(node.x, node.y, node.z)
+			graph.cameraPosition(
+				{x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio}, // new position
+				node, // lookAt ({ x, y, z })
+				3000 // ms transition duration
+			)
+		})
+	axes()
+	legend()
+	/**
+	 * Highlight those links (i.e. makes them thicker) that connect to the node being hovered over
+	 * @param {object} node
+	 */
+	function doHover(node) {
+		// no state change
+		if ((!node && !highlightNodes.size) || (node && hoverNode === node)) return
+
+		highlightNodes.clear()
+		highlightLinks.clear()
+		if (node) {
+			highlightNodes.add(node)
+			if (node.neighbors) node.neighbors.forEach((neighbor) => highlightNodes.add(neighbor))
+			if (node.links) node.links.forEach((link) => highlightLinks.add(link))
+		}
+		hoverNode = node || null
+
+		// trigger update of highlighted objects in scene
+		graph
+			.nodeColor(graph.nodeColor())
+			.linkWidth(graph.linkWidth())
+			.linkDirectionalParticles(graph.linkDirectionalParticles())
+	}
+	// fit graph to window on double click
+	listen('3dgraph', 'dblclick', () => {
+		graph.width(width).height(height).zoomToFit(200, 0)
+	})
+	// change background colour using select menu in nav bar
+	listen('mode', 'change', (e) => {
+		setMode(e.target.value)
+	})
+	window.onresize = function () {
+		display(setMode(elem('mode').value))
+	}
+	/**
+	 * set the graph background colour
+	 * @param {string} mode -  dark or light
+	 */
+	function setMode(mode) {
+		if (mode === 'dark') {
+			graph.backgroundColor('black')
+			elem('info').style.color = 'white'
+		} else {
+			graph.backgroundColor('white')
+			elem('info').style.color = 'black'
+		}
+	}
+	/**
+	 * draw axes across scene
+	 */
+	function axes() {
+		graph
+			.scene()
+			.add(
+				new THREE.Line(
+					new THREE.BufferGeometry().setFromPoints([
+						new THREE.Vector3(0, 0, -1000),
+						new THREE.Vector3(0, 0, 1000),
+					]),
+					new THREE.LineBasicMaterial({color: 'blue'})
+				)
+			)
+			.add(
+				new THREE.Line(
+					new THREE.BufferGeometry().setFromPoints([
+						new THREE.Vector3(-1000, 0, 0),
+						new THREE.Vector3(1000, 0, 0),
+					]),
+					new THREE.LineBasicMaterial({color: 'red'})
+				)
+			)
+			.add(
+				new THREE.Line(
+					new THREE.BufferGeometry().setFromPoints([
+						new THREE.Vector3(0, -1000, 0),
+						new THREE.Vector3(0, 1000, 0),
+					]),
+					new THREE.LineBasicMaterial({color: 'green'})
+				)
+			)
+	}
 }
 
 /**
@@ -184,6 +354,7 @@ function legend() {
 		.map((a) => a[1].edge.groupLabel)
 		.filter((a) => a !== 'Sample')
 	let nItems = nodes.length + edges.length
+	if (nItems == 0) return
 	let legendBox = document.createElement('div')
 	legendBox.className = 'legend'
 	legendBox.id = 'legendBox'
@@ -195,7 +366,8 @@ function legend() {
 	legendBox.appendChild(title)
 	let boxheight = LEGENDSPACING * nItems + title.offsetHeight
 	let threeDGraphDiv = elem('3dgraph')
-	legendBox.style.height = (boxheight < threeDGraphDiv.clientHeight - 100 ? boxheight : threeDGraphDiv.clientHeight - 100) + 'px'
+	legendBox.style.height =
+		(boxheight < threeDGraphDiv.clientHeight - 100 ? boxheight : threeDGraphDiv.clientHeight - 100) + 'px'
 	legendBox.style.width = HALFLEGENDWIDTH * 2 + 'px'
 	let canvas = document.createElement('div')
 	canvas.className = 'legendCanvas'
@@ -269,148 +441,4 @@ function legend() {
 		height += 50
 	}
 	legendNetwork.fit({})
-}
-
-/**
- * Display the map as a 3D network
- */
-function display() {
-	convertData()
-	cancelLoading()
-	let threeDGraphDiv = elem('3dgraph')
-	let width = threeDGraphDiv.clientWidth
-	let height = threeDGraphDiv.clientHeight
-	const highlightNodes = new Set()
-	const highlightLinks = new Set()
-	let hoverNode = null
-	graphEdges.forEach((link) => {
-		const a = graphNodes.find((n) => n.id === link.source)
-		const b = graphNodes.find((n) => n.id === link.target)
-		!a.neighbors && (a.neighbors = [])
-		!b.neighbors && (b.neighbors = [])
-		a.neighbors.push(b)
-		b.neighbors.push(a)
-
-		!a.links && (a.links = [])
-		!b.links && (b.links = [])
-		a.links.push(link)
-		b.links.push(link)
-	})
-
-	graph = ForceGraph3D()(threeDGraphDiv)
-		.width(width)
-		.height(height)
-		.graphData({nodes: graphNodes, links: graphEdges})
-		.showNavInfo(false)
-		.linkWidth((link) => (highlightLinks.has(link) ? 1 : 0))
-		.linkDirectionalArrowLength(2)
-		.linkDirectionalArrowRelPos(1)
-		.backgroundColor('white')
-		.nodeOpacity(1.0)
-		.linkOpacity(0.7)
-		.linkDirectionalParticles(5)
-		.nodeThreeObjectExtend(false)
-		.nodeThreeObject((node) => {
-			// extend node with text sprite
-			const sprite = new SpriteText(`${node.label}`)
-			sprite.color = node.fontColor
-			sprite.textHeight = 3
-			sprite.padding = 1
-			sprite.backgroundColor = node.color
-			sprite.borderWidth = 1
-			sprite.borderRadius = 4
-			return sprite
-		})
-		.onNodeDragEnd((node) => {
-			if (node.fx) {
-				// unfix fixed node
-				node.fx = null
-				node.fy = null
-				node.fz = null
-			} else {
-				// fix nodes if they have been dragged
-				node.fx = node.x
-				node.fy = node.y
-				node.fz = node.z
-			}
-		})
-		.onNodeHover(doHover)
-		.onNodeClick((node) => {
-			// Aim at node from outside it
-			const distance = 80
-			const distRatio = 1 + distance / Math.hypot(node.x, node.y, node.z)
-			graph.cameraPosition(
-				{x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio}, // new position
-				node, // lookAt ({ x, y, z })
-				3000 // ms transition duration
-			)
-		})
-	axes()
-	legend()
-
-	function doHover(node) {
-		// no state change
-		if ((!node && !highlightNodes.size) || (node && hoverNode === node)) return
-
-		highlightNodes.clear()
-		highlightLinks.clear()
-		if (node) {
-			highlightNodes.add(node)
-			if (node.neighbors) node.neighbors.forEach((neighbor) => highlightNodes.add(neighbor))
-			if (node.links) node.links.forEach((link) => highlightLinks.add(link))
-		}
-		hoverNode = node || null
-		updateHighlight()
-
-		function updateHighlight() {
-			// trigger update of highlighted objects in scene
-			graph
-				.nodeColor(graph.nodeColor())
-				.linkWidth(graph.linkWidth())
-				.linkDirectionalParticles(graph.linkDirectionalParticles())
-		}
-	}
-	document.addEventListener('dblclick', () => {
-		graph.width(width).height(height).zoomToFit(200, 0)
-	})
-	listen('mode', 'change', (e) => {
-		if (e.target.value === 'dark') graph.backgroundColor('black')
-		else graph.backgroundColor('white')
-	})
-	/**
-	 * draw axes across scene
-	 */
-	function axes() {
-		graph
-			.scene()
-			.add(
-				new THREE.Line(
-					new THREE.BufferGeometry().setFromPoints([
-						new THREE.Vector3(0, 0, -1000),
-						new THREE.Vector3(0, 0, 1000),
-					]),
-					new THREE.LineBasicMaterial({color: 'blue'})
-				)
-			)
-			.add(
-				new THREE.Line(
-					new THREE.BufferGeometry().setFromPoints([
-						new THREE.Vector3(-1000, 0, 0),
-						new THREE.Vector3(1000, 0, 0),
-					]),
-					new THREE.LineBasicMaterial({color: 'red'})
-				)
-			)
-			.add(
-				new THREE.Line(
-					new THREE.BufferGeometry().setFromPoints([
-						new THREE.Vector3(0, -1000, 0),
-						new THREE.Vector3(0, 1000, 0),
-					]),
-					new THREE.LineBasicMaterial({color: 'green'})
-				)
-			)
-	}
-	window.graphNodes = graphNodes
-	window.graphEdges = graphEdges
 }
