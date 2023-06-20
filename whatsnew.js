2195a5e0514994d3b5924bab14b47b22c8b1ff5b
Nigel Gilbert <n.gilbert@surrey.ac.uk>
2023-06-20 23:03:23 +0100

    Finished off What's New panel and entered content

---
+++ whatsnew.js
@@ -0,0 +1,10 @@
+/**
+ * This file should contain a list of descriptions of what is new in the current version.  
+ * Each item should start with <li>
+ */
+
+document.write(`
+<li>  This "What's New" panel will pop up whenever a new version of PRSM is released.  You'll only see it once.  Click the Continue button to dismiss it.
+<li> There is now a panel into which you can write a description or notes about the map as a whole.  To view the panel, click on the small grey button on the left edge of the map area.  While entering or editing text, you can use the formatting buttons at the top of the panel: bold, italic, underline, as well as change font size, insert images etc.  The contents of the panel are shared with other participants as you write. Click on the grey button again to put the panel away.
+<li> Instead of some factors and links being hidden when using the Analysis features, they are faded out, to provide more context around those that are shown.
+`)
