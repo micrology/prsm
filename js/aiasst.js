d3d02fc9240d445f42b2aa6da6dc5bae9d367d3f
Nigel Gilbert <scs1ng@surrey.ac.uk>
2026-08-19 21:19:52 +0100

    rationalise naming of CSS classes for AI Assist

--- js/aiasst.js
+++ js/aiasst.js
@@ -1,13 +1,17 @@
+import {elem, dragElement} from './utils.js'
+
 export function openAIAsstDialog() {
-	const toggleBtn = document.getElementById("toggle-chat-btn")
-	const closeBtn = document.getElementById("close-chat-btn")
-	const chatDialog = document.getElementById("chat-dialog")
+	const toggleBtn = elem('toggle-aiassistant-btn')
+	const closeBtn = elem('close-aiassistant-btn')
+	const chatDialog = elem('aiassistant-dialog')
+
+    dragElement(elem('ai-assistant-container'), elem('aiassistant-header'))
 
 	// Open / Close Toggle mechanism
 	function toggleChat() {
-		chatDialog.classList.toggle("hidden")
+		chatDialog.classList.toggle('hidden')
 	}
 
-	toggleBtn.addEventListener("click", toggleChat)
-	closeBtn.addEventListener("click", toggleChat)
+	toggleBtn.addEventListener('click', toggleChat)
+	closeBtn.addEventListener('click', toggleChat)
 }
